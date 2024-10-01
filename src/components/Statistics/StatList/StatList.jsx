89910047d50f160cd4b3f56d5199b8710628a853
Evalac <maslenokprograming.m@gmail.com>
2024-10-01 21:03:15 +0300

    need to fix homeWork2 propTypes

--- src/components/Statistics/StatList/StatList.jsx
+++ src/components/Statistics/StatList/StatList.jsx
@@ -1,10 +1,18 @@
+import css from '../Statistics.module.css';
+import PropTypes from 'prop-types';
+
 function StatList({ label, percentage }) {
   return (
-    <li className="item">
-      <span className="label">{label}</span>
-      <span className="percentage">{percentage}</span>
+    <li className={css.item}>
+      <span className={css.label}>{label}</span>
+      <span className={css.percentage}>{percentage}</span>
     </li>
   );
 }
 
 export { StatList };
+
+StatList.propTypes = {
+  label: PropTypes.string.isRequired,
+  percentage: PropTypes.number.isRequired,
+};
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,12 @@
 import { StatList } from './StatList/StatList';
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
 
 function Statistic({ title, stats }) {
   return (
-    <section className="statistics">
-      <h2 className={title}>Upload stats</h2>
-      <ul className="stat-list">
+    <section className={css.statistics}>
+      <h2 className={css.title}>{title.length > 0 ? title : ''}</h2>
+      <ul className={css.statList}>
         {stats.map(stat => (
           <StatList
             key={stat.id}
@@ -16,5 +18,15 @@ function Statistic({ title, stats }) {
     </section>
   );
 }
+
+Statistic.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 export { Statistic };
