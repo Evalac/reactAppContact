f578d50e8c6fc1809a16f16c343daa4152334482
Evalac <maslenokprograming.m@gmail.com>
2024-10-03 19:56:24 +0300

    props types fixed, don`t use props types key

--- src/components/Statistics/StatList/StatList.jsx
+++ src/components/Statistics/StatList/StatList.jsx
@@ -1,9 +1,11 @@
 import css from '../Statistics.module.css';
 import PropTypes from 'prop-types';
+import { getRandomHexColor } from 'utils/randomColorForStatList';
 
 function StatList({ label, percentage }) {
+  const backgroundColor = getRandomHexColor();
   return (
-    <li className={css.item}>
+    <li className={css.item} style={{ backgroundColor }}>
       <span className={css.label}>{label}</span>
       <span className={css.percentage}>{percentage}</span>
     </li>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -23,6 +23,7 @@ Statistic.propTypes = {
   title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.exact({
+      id: PropTypes.string.isRequired,
       label: PropTypes.string.isRequired,
       percentage: PropTypes.number.isRequired,
     })
