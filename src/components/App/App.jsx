71cb667e589a5f843fbaabb67c054acdd8ea4911
Evalac <maslenokprograming.m@gmail.com>
2024-11-20 20:09:48 +0200

    start module 2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,9 +8,44 @@ import { FriendList } from '../FriendList/FriendList';
 import { TransactionHistory } from '../TransactionHistory/TransactionHistory';
 import { Container } from './App.styled';
 
+import React, { Component } from 'react';
+
+// ✅ Добре
+class Counter extends Component {
+  /* ... */
+
+  handleIncrement = evt => {
+    console.log('Increment button was clicked!', evt); // працює
+    console.log('this.props: ', this.props); // працює
+  };
+
+  handleDecrement = evt => {
+    console.log('Decrement button was clicked!', evt); // працює
+    console.log('this.props: ', this.props); // працює
+  };
+
+  render() {
+    const { step } = this.props;
+
+    return (
+      <div>
+        <span>0</span>
+        <button type="button" onClick={this.handleIncrement}>
+          Increment by {step}
+        </button>
+
+        <button type="button" onClick={this.handleDecrement}>
+          Decrement by {step}
+        </button>
+      </div>
+    );
+  }
+}
+
 export const App = () => {
   return (
     <>
+      <Counter step={5} />
       <Profile
         username={users.username}
         tag={users.tag}
