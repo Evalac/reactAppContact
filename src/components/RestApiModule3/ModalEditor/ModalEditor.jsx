fe42c52bb08b544d00fdd7e65726fa6a410502d6
Evalac <maslenokprograming.m@gmail.com>
2025-01-22 17:56:29 +0200

    modal

---
+++ src/components/RestApiModule3/ModalEditor/ModalEditor.jsx
@@ -0,0 +1,10 @@
+function MoadlEditor(params) {
+  return (
+    <div>
+      <button>Тепер точно редагувати </button>
+      <button>Закрити </button>
+    </div>
+  );
+}
+
+export { MoadlEditor };
